25223d467df0b74f97040e33507746170e831fb7
FunnyValentine <darkbrewx@gmail.com>
2025-08-04 19:39:30 +0900

    Initial Commit

---
+++ MyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyDemo
+//
+//  Created by Funny Valentine. on 2025/08/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Funny Valentine. on 2025/08/04.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
